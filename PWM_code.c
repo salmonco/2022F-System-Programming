98096f3288eb4b68384025ae675b00a2b704db03
swsyp 12345 <softwaresyp333@ajou.ac.kr>
2022-12-09 14:07:15 +0900

    Update PWM_code.c

--- PWM_code.c
+++ PWM_code.c
@@ -299,7 +299,8 @@ int main(int argc, char *argv[]) {
 	case_h = atoi(msg2);
 	
 	int v, h;	// v = vertical, h = horizontal
-	
+	v=0;
+	h=0;
 	// PWM0(=GPIO 18), PWM1(=GPIO 13)
 	switch(case_v&&case_h)
 	{
